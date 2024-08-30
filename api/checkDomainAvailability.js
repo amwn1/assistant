066c7213c719cde46c5e0de7fe8fdf1b3d4d395c
amwn1 <amanmalik3105@gmail.com>
2024-08-30 15:10:29 +0530

    v13

--- api/checkDomainAvailability.js
+++ api/checkDomainAvailability.js
@@ -1,7 +1,7 @@
 // api/checkDomainAvailability.js
 export default async function handler(req, res) {
     // Set CORS headers
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://thenameexperts.com");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   
