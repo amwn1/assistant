e2b7047c6ebd9d92cdc14c38ad5bf229e3bde323
amwn1 <amanmalik3105@gmail.com>
2024-08-31 16:26:50 +0530

    ok

--- api/pusher-event.js
+++ api/pusher-event.js
@@ -1,12 +1,60 @@
-// Import the URLSearchParams from 'url' module
-const { URLSearchParams } = require('url');
+// import Pusher from 'pusher';
+
+// const pusher = new Pusher({
+//   appId: process.env.PUSHER_APP_ID,
+//   key: process.env.PUSHER_APP_KEY,
+//   secret: process.env.PUSHER_APP_SECRET,
+//   cluster: process.env.PUSHER_CLUSTER,
+//   useTLS: true,
+// });
+
+// export default async function handler(req, res) {
+//   try {
+//     if (req.method !== 'POST') {
+//       console.log('Invalid method:', req.method);
+//       return res.status(405).json({ message: 'Method not allowed' });
+//     }
+
+//     // Use the built-in Vercel parsing method to manually parse JSON
+//     let body;
+//     try {
+//       body = JSON.parse(req.body);
+//     } catch (error) {
+//       console.error('Error parsing JSON:', error, 'Received body:', req.body);
+//       return res.status(400).json({ message: 'Invalid JSON format' });
+//     }
+
+//     const { message } = body;
+
+//     if (!message) {
+//       console.log('No message provided in request body:', body);
+//       return res.status(400).json({ message: 'Bad request, no message provided' });
+//     }
+
+//     try {
+//       await pusher.trigger('my-channel', 'my-event', { message: message });
+//       res.status(200).json({ message: 'Event triggered successfully' });
+//     } catch (error) {
+//       console.error('Error triggering Pusher event:', error);
+//       res.status(500).json({ message: 'Internal Server Error' });
+//     }
+
+//   } catch (error) {
+//     console.error('Unexpected error:', error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// }
+
+
+// This variable will temporarily store the data
+import { URLSearchParams } from 'url';
 
 // Temporary in-memory storage
 let latestMessage = '';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://thenameexperts.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://thenameexperts.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
@@ -46,6 +94,7 @@ module.exports = async function handler(req, res) {
       latestMessage = message;
 
       return res.status(200).json({ message: 'Data received successfully' });
+
     } else if (req.method === 'GET') {
       // Serve the latest message and then clear it
       if (!latestMessage) {
@@ -66,4 +115,4 @@ module.exports = async function handler(req, res) {
     console.error('Unexpected error:', error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
-};
+}
