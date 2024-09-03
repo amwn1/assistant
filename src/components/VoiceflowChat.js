ef3ebed07f14a7599b220cf3b5d88881b58d270b
amwn1 <amanmalik3105@gmail.com>
2024-09-03 13:32:21 +0530

    env variables

--- src/components/VoiceflowChat.js
+++ src/components/VoiceflowChat.js
@@ -7,7 +7,7 @@ const VoiceflowChat = () => {
     script.type = "text/javascript";
     script.onload = () => {
       window.voiceflow.chat.load({
-        verify: { projectID: process.env.VOICEFLOW_VERSION_ID },
+        verify: { projectID: "66cd6015e166995d728b65f7"},
         url: 'https://general-runtime.voiceflow.com',
         versionID: 'development'
         // render: {
